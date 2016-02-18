bb51c9c891b25b5fc75e7095579a95c8511f86ee
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 17:58:28 +0100

    Initial Commit

---
+++ Image/Image.h
@@ -0,0 +1,19 @@
+//
+//  Image.h
+//  Image
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Image.
+FOUNDATION_EXPORT double ImageVersionNumber;
+
+//! Project version string for Image.
+FOUNDATION_EXPORT const unsigned char ImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Image/PublicHeader.h>
+
+
